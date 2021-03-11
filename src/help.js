e4d8942496d087551d23fccd867d59d84fd554c6
Felipe Peres <peres.lipe@gmail.com>
2021-03-11 00:38:57 -0300

    botzin

---
+++ src/help.js
@@ -0,0 +1,105 @@
+const help = (prefix) => {
+	return `
+╔══✪〘 INFO BOT〙
+║
+╠➥ Nome: *Woody Bot*
+╠➥ V: *1.0*
+╠➥ *Criador: Herbson🕵🏽‍♂️*
+╠➥ Status Bot: ON
+
+
+
+╠══✪〘 MENU DE FIGURINHAS〙
+
+🎆 !fig = *transforma imagem em figurinha.*
+
+🤹‍♂️ !fgf = *transforma gif em figurinha*
+Obs: o vídeo/gif, deve ter no máximo 2 segundos de duração.
+
+🔁 !fimg = *transforma figurinha em imagem.*
+
+
+╠══✪〘MENU DE MEMES〙
+
+😂 !meme = *bot manda um meme aleatório*
+
+
+╠══✪〘 MENU DE ÁUDIO 〙
+
+🗣️ !tts pt = *manda áudio com voz do Google.*
+Ex: !tts pt olá 
+
+
+╠══✪〘 MENU DE GRUPOS 〙
+
+*OBS: PARA QUE ESSES COMANDOS FUNCIONEM, O BOT TEM QUE SER ADM DO GP*
+
+👺 !link = *manda link do grupo*
+
+🗣️ !marcartodos = *o bot marca todos os participantes do grupo*
+
+📝 !ginfo = *o bot manda todas informações do grupo*
+
+
+╠══✪〘 MENU PARA ADM DO GRUPO
+
+✏️ !add 558398669xxxx = *adiciona participante*
+
+😡 !banir @membro = *bani o membro do grupo*
+
+⬆️ !adm @membro = *dar adm ao membro marcado*
+
+ ⬇️ !rebaixar @membro = *rebaixa o membro com cargo de Adm*
+
+🎉 !bemvindo = *ativa o texto de boas vindas no grupo*
+
+!antiracismo on/off = *o bot ira banir o usuário que praticar racismo no grupo*
+
+!antilink on/off = *o bot ira banir usuários que compartilhar  link de outro gp*
+
+
+╠══✪〘 MENU DE IMAGENS +🔞〙
+
+🔞 !hentai
+🔞 !porno
+
+
+╠══✪〘 MENU ANIME〙
+
+!minato = *(manda uma foto minato aleatória)*
+
+!boruto = *manda imagem boruto (aleatória)*
+
+!hinata = *manda uma foto hinata (aleatória)*
+
+!sasuke = *manda uma imagem sasuke (aleatória)*
+
+!sakura = *manda uma foto sakura (aleatória)*
+
+!naruto = *manda uma foto naruto (aleatória)*
+
+
+╠══✪〘 DONO/BOT 〙
+
+!bug = *informe ao dono algum bug*
+Ex: !bug não consigo fazer figurinha
+
+!Infodono = *informações do criador*
+
+!ping = *ver velocidade do bot*
+
+
+╠══✪〘 DONO 〙✪
+║
+║ *NOME: Herbson 😎*
+║ *INSTA: euherbson*
+║
+║ *em breve nova ║atualização!*
+║
+║ *Peita ou respeita*🤪
+║
+║
+╚═〘 Obrigado por usar Woody Bot 〙🤹‍♂️🔥
+
+
+exports.help = help
